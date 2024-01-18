c60f1880f4dde4857f2b6b94e368eeb38209da82
Hadsonpar <hadson1@gmail.com>
2024-01-18 11:50:14 -0500

    Python if, else and elif conditional statements

---
+++ 05.Control_flows/conditionals.py
@@ -0,0 +1,56 @@
+# Python if, else and elif conditional statements
+# ---------------------------------------------------------------------
+
+# =====================================================================
+# 1. Python if conditional statement
+"""
+Syntax if
+if condition:
+    body of if statement
+"""
+
+# Example if conditional statement
+x = 100
+if x == 100:
+    print("if: The condition is ", x)
+# =====================================================================
+
+# =====================================================================
+# 2. Python else conditional statement
+"""
+Syntax else
+if condition:
+    body of if statement
+else:
+    body of else statement
+"""
+
+# Example else conditional statement
+x = 100
+if x < 100:
+    print("if...else: It is a number is less 100", x)
+else:
+    print("if...else: The defined condition is ", x)
+# =====================================================================
+
+# =====================================================================
+# 3. Python elif conditional statement
+"""
+Syntax elif conditional statement:
+if condition1:
+    code block 1
+elif condition2:
+    code block 2
+else: 
+    code block 3
+"""
+
+# Example elif conditional statements
+z = 10
+if z == 10:
+    print("if...else: The number is", z)
+elif z < 10:
+    print("if...else: The number is", z)
+else: 
+    print("if...else: The number is", z)
+# =====================================================================
